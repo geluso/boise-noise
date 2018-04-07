0478390b5aa0b54283274cf2c3c35a3a345cda33
Steve Geluso <stevegeluso@gmail.com>
2018-04-07 16:11:55 -0700

    mongodb multi reply

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
+
+const Contact = mongoose.model('Contact', {number: String});
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -11,6 +16,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
+
+  let number = req.body.From;
+  console.log('got', number, req.body.Body);
+
+  Contact.find({number})
+  .then(contact => {
+    if (contact === null) {
+      return Contact.create({number});
+    }
+    return contact;
+  })
+  .then(contact => {
+    return Contact.find({});
+  })
+  .then(contacts => {
+    contacts.forEach(contact => {
+      console.log('contact:', contact.number);
+      if (contact.number !== number) {
+        console.log('send to', contact.number);
+        // client.messages
+        // .create({
+        //   to: contact.number,
+        //   from: process.env.TWILIO_NUMBER,
+        //   body: 'mass text',
+        // })
+        // .then(message => console.log(message.sid));
+      }
+    });
+  })
+  .catch(err => {
+    res.status(500).send(err);
+    return;
+  });
 
   if (req.body.Body == 'hello') {
     twiml.message('hi!');
